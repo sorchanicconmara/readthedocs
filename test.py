712d7e09d87e1e125e9d3e2fa2d4eb7ec63a3a44
sorchanicconmara <sorcha.nicconmara@ucdconnect.ie>
2019-08-21 10:19:57 +0100

    Update test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# Python3 program to add two numbers
-num1 = 15
-num2 = 12
-
-# Adding two numbers
-sum = num1 + num2
-
-# printing values
-print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
